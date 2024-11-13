7711dbc145f610966f6e92db50de6fbc383adcf1
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-13 07:37:58 -0500

    update

--- src/main/java/com/example/classwork/buttonController.java
+++ src/main/java/com/example/classwork/buttonController.java
@@ -7,10 +7,7 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.text.Text;
 
 public class buttonController {
