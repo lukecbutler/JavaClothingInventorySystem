9b7cc69fd36d3dd8a092e44b88abb4b07ba5d5b8
tarheelfan200 <tarheelfan3951@gmail.com>
2024-11-17 11:05:42 -0500

    update controller

--- src/main/java/com/example/classwork/guiController.java
+++ src/main/java/com/example/classwork/guiController.java
@@ -3,21 +3,59 @@ package com.example.classwork;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
 
 public class guiController {
 
-    @FXML
-    public Button buttonID;
+    // create list of apparel items to be passed into inventory
+    ArrayList<Apparel> apparelItems = new ArrayList<>();
+    // create inventory object - storing apparel items
+    Inventory inventory = new Inventory(apparelItems);
+
 
     @FXML
-    private TextField userInput;
+    private TextField productNameField;
 
     @FXML
-    private TextField userOutput;
+    public TextField quantityField;
+
+    @FXML
+    private TextField sizeField;
+
+    @FXML
+    public TextField categoryField;
+
+    @FXML
+    public TextField brandField;
+
+    @FXML
+    public TextField priceField;
+
+    @FXML
+    public Button addItemButton;
+
 
     @FXML
     private void displayUserInput(ActionEvent event){
-        userOutput.setText(String.valueOf(userInput.getText()));
+        //size.setText(String.valueOf(productID.getText()));
     }
+
+    @FXML
+    private void handleCreateProduct() {
+        String productName = productNameField.getText();
+        int quantity = Integer.parseInt(quantityField.getText());
+        String size = sizeField.getText();
+        String category = categoryField.getText();;
+        String brand = brandField.getText();
+        double price = Double.parseDouble(priceField.getText());
+
+        Apparel newItem = new Apparel(productName, quantity, size, category, brand, price);
+
+        inventory.addItem(newItem);
+
+    }
+
 
 }
